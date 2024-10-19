ef8207018c2732ef22f291d339f6a5950b720e92
Ulugbek Suleymonov <ulugbeksuleymonov01@gmail.com>
2024-10-19 23:45:16 +0500

    create database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from models import db, User, Score
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -8,4 +9,9 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 # Configure the database for app
-app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db.init_app(app)    # initializes Flask app (app) for use with extension (db, which is imported from models.py)
+
+with app.app_context():
+    db.create_all   # creates tables
