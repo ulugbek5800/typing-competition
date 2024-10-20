534afa35d7376ba9c7b9d496d4bd64ff304fdffc
Ulugbek Suleymonov <ulugbeksuleymonov01@gmail.com>
2024-10-20 17:44:58 +0500

    Add app_context, .gitignore, new routes

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 # Initialize Flask app
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
 # Configure the database for app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLAlchemy_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)    # initializing the database
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
